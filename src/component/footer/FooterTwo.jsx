fb887ba26126cdae4119930922b409d8bbed0369
cmouli2306 <51374679+cmouli2306@users.noreply.github.com>
2020-11-26 07:31:05 -0600

    Update FooterTwo.jsx

--- src/component/footer/FooterTwo.jsx
+++ src/component/footer/FooterTwo.jsx
@@ -14,15 +14,6 @@ const FooterTwo = () => {
             <div className="wrapper plr--50 plr_sm--20">
                 <div className="row align-items-center justify-content-between">
                     <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div className="inner">
-                            <div className="logo text-center text-sm-left mb_sm--20">
-                                <a href="/home-one">
-                                    <img src="/assets/images/logo/logo.png" alt="Logo images"/>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-6 col-12">
                         <div className="inner text-center">
                             <ul className="social-share rn-lg-size d-flex justify-content-center liststyle">
                                 {SocialShare.map((val , i) => (
@@ -34,7 +25,7 @@ const FooterTwo = () => {
                     <div className="col-lg-4 col-md-12 col-sm-12 col-12">
                         <div className="inner text-lg-right text-center mt_md--20 mt_sm--20">
                             <div className="text">
-                                <p>Copyright © 2020 Rainbow-Themes. All Rights Reserved.</p>
+                                <p>Copyright © 2020 Onuslaundry. All Rights Reserved.</p>
                             </div>
                         </div>
                     </div>
